f6e9dca0e5097064b9fbc4ec49668b923bcf1385
thekamakaji <43650594+thekamakaji@users.noreply.github.com>
2019-10-04 17:58:31 -0400

    Created Vector C Project

---
+++ Sensors and Data Acquisition/eclipse-workspace/Vector/src/main.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Author      : Jeffrey Kaji
+ Version     :
+ Copyright   : Intended for use by Purdue Space Program Solids
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
