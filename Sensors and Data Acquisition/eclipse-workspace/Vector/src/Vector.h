8cb8f89103deb9438a2f6fef01ea03201b524597
thekamakaji <43650594+thekamakaji@users.noreply.github.com>
2019-10-07 18:07:42 -0400

    Added Matrix class

--- Sensors and Data Acquisition/eclipse-workspace/Vector/src/Vector.h
+++ Sensors and Data Acquisition/eclipse-workspace/Vector/src/Vector.h
@@ -2,7 +2,7 @@
  * Vector.h
  *
  *  Created on: Oct 4, 2019
- *      Author: Jeff
+ *      Author: Jeffrey Kaji
  */
 
 #ifndef VECTOR_H_
@@ -17,6 +17,25 @@ Vector* subV(Vector* v0, Vector* v1);
 Vector* mulV(Vector* v0, double d);
 Vector* divV(Vector* v0, double d);
 
+double dotV(Vector* v0, Vector* v1);
+Vector* crossV(Vector* v0, Vector* v1);
+
+double getVVal(Vector* v0, int n);
+double getVMag(Vector* v0);
+Vector* getVUnit(Vector* v0);
+double getVUnitVal(Vector* v0, int n);
+
+double getVAngle(Vector* v0, Vector* v1);
+double getVAngleDeg(Vector* v0, Vector* v1);
+
+Vector* projV(Vector* target, Vector* v);
+Vector* projP(Vector* normal, Vector* v);
+
 void printV(Vector* v);
+
+
+double deg2rad(double deg);
+double rad2deg(double rad);
+
 
 #endif /* VECTOR_H_ */
