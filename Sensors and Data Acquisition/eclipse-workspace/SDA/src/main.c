dff2844d8e38f4dbd75f3f3ae609404957d9e82b
thekamakaji <43650594+thekamakaji@users.noreply.github.com>
2019-10-10 16:27:29 -0400

    Cleaned main.c
    Got rid of test code

--- Sensors and Data Acquisition/eclipse-workspace/SDA/src/main.c
+++ Sensors and Data Acquisition/eclipse-workspace/SDA/src/main.c
@@ -18,18 +18,5 @@ int main(void) {
 	setup();
 
 
-	Vector* v0 = Vector_new(0,0,0);
-	Vector* v1 = Vector_new(3,4,5);
-	Vector* v2 = Vector_new(5,3,2);
-	Matrix* m0 = MatrixRef_new(v1,v2);
-	Vector* v3 = Vector_new(-3,2,9);
-
-	Vector* v4 = mulMV(m0,v3);
-
-
-
-
-	printV(v1);
-
 	return EXIT_SUCCESS;
 }
